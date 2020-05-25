0d6f1fec05e5d003e065c0b24c9606d52909124e
Jesus Gomez <jesgombel@outlook.es>
2020-05-25 15:05:40 +0200

    feat(app): show result

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,11 @@
 /** @jsx j */
-import { enableHooks, render, j } from '@evuz/joltik'
+import { enableHooks, render, j, createElement } from '@evuz/joltik'
 
 import { decrypt } from './helpers/decrypt.js'
 import { encrypt } from './helpers/encrypt.js'
 import { Button } from './components/Button'
 import { Input } from './components/Input'
+import { Snippet } from './components/Snippet.jsx'
 
 import './styles'
 
@@ -13,7 +14,7 @@ enableHooks()
 
 render(<Button onClick={handleEncrypt}>Encrypt!</Button>, document.getElementById('buttons'))
 render(<Button secondary onClick={handleDecrypt}>Decrypt!</Button>, document.getElementById('buttons'))
-render(<Input placeholder='Insert text' onChange={saveInput('content')}>Decrypt!</Input>, document.getElementById('input'))
+render(<Input area placeholder='Insert text' onChange={saveInput('content')}>Decrypt!</Input>, document.getElementById('input'))
 render(<Input placeholder='Secret key' onChange={saveInput('secret')} type='password'>Decrypt!</Input>, document.getElementById('secret'))
 
 function saveInput (key) {
@@ -24,9 +25,19 @@ function saveInput (key) {
 
 async function handleEncrypt () {
   const encryptedContent = window.btoa(await encrypt(inputs.content, inputs.secret))
-  console.log(encryptedContent)
+  renderSnippetContent(encryptedContent)
 }
 
 async function handleDecrypt () {
-  console.log(await decrypt(window.atob(inputs.content), inputs.secret))
+  const decryptedContent = await decrypt(window.atob(inputs.content), inputs.secret)
+  renderSnippetContent(decryptedContent)
+}
+
+function renderSnippetContent (text) {
+  const mount = (el) => (
+    document.getElementById('result').firstChild
+      ? document.getElementById('result').firstChild.replaceWith(el)
+      : document.getElementById('result').appendChild(el)
+  )
+  mount(createElement(<Snippet text={text} />))
 }
