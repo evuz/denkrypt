0ede967db5fe08897f96b2305f55c4bbe1f192ac
Jesus Gomez <jesgombel@outlook.es>
2020-05-27 18:40:08 +0200

    fix: update images path

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -3,6 +3,6 @@ import { j } from '@evuz/joltik'
 
 export function Icon ({ name }) {
   return (
-    <img className='Icon' src={`public/feather/${name}.svg`} />
+    <img className='Icon' src={`/public/feather/${name}.svg`} />
   )
 }
