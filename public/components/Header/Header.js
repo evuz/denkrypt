2f2d34005395042cb736f60dbc6a69d331d77ef3
Jesús Gómez Belindo <jesus.gomezb@sngular.com>
2020-12-09 17:21:43 +0100

    fix: add height and width img element

--- public/components/Header/Header.js
+++ public/components/Header/Header.js
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 export function Header () {
   return (
     <header className={styles.Header}>
-      <img className={styles.logo} src='/assets/logo.png' alt='denkrypt logo' />
+      <img height='80' width='80' className={styles.logo} src='/assets/logo.png' alt='denkrypt logo' />
       <h1 className={styles.title}>Welcome to Denkrypt!</h1>
     </header>
   )
