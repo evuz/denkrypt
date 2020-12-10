03be25b66c442cb86f57cb5f1acc1c16f286be95
Jesús Gómez Belindo <jesus.gomezb@sngular.com>
2020-12-10 10:23:37 +0100

    fix(snippet): copy to clipboard

--- public/components/Snippet/Snippet.js
+++ public/components/Snippet/Snippet.js
@@ -1,12 +1,13 @@
 import styles from './Snippet.module.css'
 
+import { copyToClipboard } from '../../utils/copyClipboard'
 import { CopyIcon } from '../Icons/Copy.icon'
 
 export function Snippet ({ text }) {
   return (
     <div className={styles.Snippet}>
       {text}
-      <button className={styles.copy}>
+      <button className={styles.copy} onClick={() => copyToClipboard(text)}>
         <CopyIcon size={1.5} />
       </button>
     </div>
