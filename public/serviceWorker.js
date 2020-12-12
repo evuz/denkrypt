3b7003e03aeebafcf2bdbbd6ac06b478b6c1af72
Jesus Gomez <jesgombel@outlook.es>
2020-12-12 19:46:14 +0100

    fix(service worker): remove log & cache index.html

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -8,8 +8,6 @@ self.addEventListener('install', (event) =>
 )
 
 async function installServiceWorker () {
-  log('Service Worker installation started ')
-
   const cache = await caches.open(getCacheName())
 
   return cache.addAll(ASSETS)
@@ -18,8 +16,6 @@ async function installServiceWorker () {
 self.addEventListener('activate', () => activateSW())
 
 async function activateSW () {
-  log('Service Worker activated')
-
   const cacheKeys = await caches.keys()
 
   cacheKeys
@@ -39,20 +35,10 @@ async function cacheThenNetwork (event) {
   const cachedResponse = await cache.match(event.request)
 
   if (cachedResponse) {
-    log('Serving From Cache: ' + event.request.url)
     return cachedResponse
   }
 
-  const networkResponse = await fetch(event.request)
-
-  log('Calling network: ' + event.request.url)
-
-  return networkResponse
-}
-
-// each logging line will be prepended with the service worker version
-function log (message) {
-  console.log(message)
+  return fetch(event.request)
 }
 
 function getCacheName () {
