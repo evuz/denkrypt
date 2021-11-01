af60d407a301840425d88058965005bbbcc93958
Jesús Gómez <jesgombel@outlook.es>
2021-11-01 10:01:58 +0100

    chore: add env variable

--- public/index.js
+++ public/index.js
@@ -15,8 +15,24 @@ export function App () {
 hydrate(<App />)
 
 export async function prerender (data) {
-  const { default: prerender } = await import('preact-iso/prerender')
-  return await prerender(<App {...data} />)
+  const { default: render } = await import('preact-iso/prerender')
+
+  const result = await render(<App {...data} />)
+  return {
+    ...result,
+    head: {
+      elements: new Set([
+        {
+          type: 'meta',
+          props: {
+            name: 'vir',
+            content: process.env.GOOGLE_VERIFICATION,
+            wmr: process.env.WMR_GOOGLE_VERIFICATION
+          }
+        }
+      ])
+    }
+  }
 }
 
 registerServiceWorker()
